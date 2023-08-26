68e216b8234eeddaf0fe083dd4a72deb68ea0722
Marvin Nyingi <nyingi.marvin@outlook.com>
2023-08-26 14:19:45 +0300

    project setup

---
+++ src/main/java/dev/marvin/savingsmanagement/SavingsManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.marvin.savingsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsManagementApplication.class, args);
+	}
+
+}
